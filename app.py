67fede796062c357830171f8b2edf7bbf61f0473
g0g3t1T <vokuzmin@edu.hse.ru>
2025-01-07 19:04:52 +0300

    0

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def login():
 # Страница успешного входа
 @app.route('/success')
 def success():
-    return "<h1>Login Successful!</h1><p>Welcome to the protected area.</p>"
-
+    render_template('main.html')
+    
 if __name__ == '__main__':
     app.run(debug=True)
