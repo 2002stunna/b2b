c64fc8b953dddebbc148a601f73bbecdc2ef061c
g0g3t1T <vokuzmin@edu.hse.ru>
2025-01-07 19:56:39 +0300

    6

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import sqlite3
 
 app = Flask(__name__)
 
-# Функция для проверки пользователя в базе данных
 def check_user(username, password):
     try:
         conn = sqlite3.connect('users.db')
@@ -11,20 +10,16 @@ def check_user(username, password):
         cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
         user = cursor.fetchone()
         conn.close()
-        print(f"Результат запроса: {user}")
         return user
     except Exception as e:
         print(f"Ошибка в check_user: {e}")
         return None
 
-# Главная страница с формой логина
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-
-        print(f"Получены данные: username={username}, password={password}")
 
         if not username or not password:
             return "Ошибка: не заполнены поля логина или пароля.", 400
@@ -36,10 +31,9 @@ def login():
 
     return render_template('login.html')
 
-# Страница успешного входа
-@app.route('/success')
+@app.route('/success', methods=['GET'])
 def success():
-    render_template('main.html')
-    
+    return render_template('main.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
